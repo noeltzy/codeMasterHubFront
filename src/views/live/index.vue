5ffb89725d62564b1c562ba43a332d6cbaaf5ba5
Noell <noeltzy@163.com>
2024-12-12 19:39:04 +0800

    add default page

--- src/views/live/index.vue
+++ src/views/live/index.vue
@@ -1,6 +1,16 @@
 <template>
   <page-layout title="直播">
-    <!-- 这里放页面特有的内容 -->
+    <el-result
+      icon="info"
+      title="开发中"
+      sub-title="教程功能正在开发中，敬请期待..."
+    >
+      <template #extra>
+        <el-button type="primary" @click="$router.push('/study')"
+          >去学习</el-button
+        >
+      </template>
+    </el-result>
   </page-layout>
 </template>
 
